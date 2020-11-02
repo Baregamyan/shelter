29c65943d6e4b7ad55927ba09e187b8380a7e83a
baregamyan <baregamyan@gmail.com>
2020-11-02 12:49:31 +0300

    feat: add focus-visible

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 import './modules/pets-slider.js';
 import './modules/smoothscroll.js';
+import 'focus-visible';
 import {NavigationMenu} from './modules/navigation.js';
 
 const navigation = new NavigationMenu(`header__navigation`, `header__trigger`);
