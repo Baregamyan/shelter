996950f960f3bc1cd3ffe797cc9ff1f5b6ae3018
baregamyan <baregamyan@gmail.com>
2020-11-06 10:41:07 +0300

    fix: correct smooth scrolling on webkit engine

--- src/js/modules/smoothscroll.js
+++ src/js/modules/smoothscroll.js
@@ -1,9 +1,12 @@
+import smoothscroll from 'smoothscroll-polyfill';
+smoothscroll.polyfill();
 document.querySelectorAll('.nav__link[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
       e.preventDefault();
+      const id = this.getAttribute('href');
+      const block = document.querySelector(id);
+      const headerHeight = document.querySelector('.header__navigation').offsetHeight;
 
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
+      window.scrollTo({top: (block.offsetTop - headerHeight), left: 0, behavior: 'smooth'});
   });
 });
